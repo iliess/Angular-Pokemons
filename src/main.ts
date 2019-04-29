c71ae7dea48d740161cc6eb5a96374bd04daaf59
Iliess67 <el-moumene@hotmail.com>
2019-04-29 14:18:53 -0400

    Ready for Deployment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
 import { enableProdMode } from  '@angular/core';
 
 if(!/localhost/.test(document.location.host)){
